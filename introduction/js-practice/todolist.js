5c8a3f56385fc55e653589818bf6b3f1f6224fa0
Wayne <tsun0227@yahoo.com.tw>
2021-01-05 14:18:20 +0800

    localstorage

--- introduction/js-practice/todolist.js
+++ introduction/js-practice/todolist.js
@@ -4,27 +4,35 @@ const sendBtn = slr("#send");
 const input = slr("#input");
 const todolist = slr("#todolist");
 const editInput = slr("#editInput");
-const completedBtn = document.querySelectorAll(".completedBtn");
+
+const addEvent4Btns = (selector, eventType, func) => {
+  document.querySelectorAll(selector).forEach((element) => {
+    element.addEventListener(eventType, (e) => func(e));
+  });
+};
 
 //存放代辦事項arr
 // {id:number , text:string, completed:false}
 let todos = [
-  //預設
-  { id: 111, text: "複習react", completed: false, edit: false },
-  { id: 222, text: "練習js", completed: true, edit: false },
+  // { "id": 111, text: "複習react", "completed": false, "edit": false },
+  // { "id": 222, text: "練習js", "completed": true, "edit": false }
 ];
 
 //新增代辦事項至todos陣列中
 const addTodo = () => {
   // {id:number , text:string, completed:false, edit:false}
-  const todosItems = {
-    id: +new Date(),
-    text: input.value,
-    completed: false,
-    edit: false,
-  };
-  todos.unshift(todosItems);
+  if (input.value.trim()) {
+    const todosItems = {
+      id: +new Date(),
+      text: input.value,
+      completed: false,
+      edit: false,
+    };
+    todos.unshift(todosItems);
+  }
   input.value = ``;
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 };
 
 //切換是否已完成
@@ -35,7 +43,7 @@ const completedToggle = (id) =>
 //刪除代辦事項
 const deleteItem = (id) => {
   todos = todos.filter((v) => v.id !== +id);
-  // todos = [...newTodos];
+  localStorage.setItem("todos", JSON.stringify(todos));
 };
 //切換編輯狀態
 const editToggle = (id) =>
@@ -44,25 +52,29 @@ const editToggle = (id) =>
     v.id == +id && (v.edit = !v.edit);
   });
 //儲存已編輯
-const saveItem = (id) =>
+const saveItem = (id) => {
   todos.forEach((v) => {
     v.id == +id &&
       (v.text = document.querySelector("#editInput").value) &&
       (v.edit = false);
   });
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
 
 //呈現畫面
 const display = () => {
   const displayTodolist = todos.map((value) => {
     let displayTodolistTmp = "";
     displayTodolistTmp = value.edit
-      ? `<li><input id="editInput" type="text" value="${value.text}"/>
+      ? `<li>
+      <input id="editInput" type="text" value="${value.text}"/>
       <button class="btn btn-primary saveBtn" id="${value.id}">save</button>
       <button class="btn btn-primary completedBtn" id="${value.id}">completed</button>
       <button class="btn btn-primary deleteBtn" id="${value.id}">delete</button>
       </li>`
       : value.completed
-      ? `<li><del>${value.text}</del>
+      ? `<li>
+      <del>${value.text}</del>
       <button class="btn btn-primary editBtn" id="${value.id}">edit</button>
       <button class="btn btn-primary completedBtn" id="${value.id}">completed</button>
       <button class="btn btn-primary deleteBtn" id="${value.id}">delete</button>
@@ -78,6 +90,8 @@ const display = () => {
 
   todolist.innerHTML = displayTodolist.join("");
 
+  //以下為各按鈕事件
+
   //完成按鈕事件
   document.querySelectorAll(".completedBtn").forEach((element) => {
     element.addEventListener("click", (e) => {
@@ -87,33 +101,27 @@ const display = () => {
   });
 
   //刪除按鈕事件
-  document.querySelectorAll(".deleteBtn").forEach((element) => {
-    element.addEventListener("click", (e) => {
-      deleteItem(e.target.id);
-      display();
-    });
+  addEvent4Btns(".deleteBtn", "click", (e) => {
+    deleteItem(e.target.id);
+    display();
   });
+
   //編輯按鈕事件
-  document.querySelectorAll(".editBtn").forEach((element) => {
-    element.addEventListener("click", (e) => {
-      editToggle(e.target.id);
-      display();
-    });
-  });
-  //儲存按鈕事件
-  document.querySelectorAll(".saveBtn").forEach((element) => {
-    element.addEventListener("click", (e) => {
-      saveItem(e.target.id);
-      display();
-    });
-  });
-  document.querySelectorAll("#editInput").forEach((element) => {
-    element.addEventListener("keypress", (e) => {
+  addEvent4Btns(".editBtn", "click", (e) => {
+    const itemId = e.target.id;
+    editToggle(e.target.id);
+    display();
+    document.querySelector("#editInput").addEventListener("keypress", (e) => {
       if (e.key === "Enter") {
-        saveItem(e.target.id);
+        saveItem(itemId);
         display();
       }
     });
+  });
+  //儲存按鈕事件
+  addEvent4Btns(".saveBtn", "click", (e) => {
+    saveItem(e.target.id);
+    display();
   });
 };
 
@@ -129,4 +137,10 @@ input.addEventListener("keypress", (e) => {
   }
 });
 
-display();
+const initTodo = () => {
+  const newTodos = JSON.parse(localStorage.getItem("todos"));
+  todos = [...newTodos];
+  display();
+};
+
+initTodo();
