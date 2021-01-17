051488dad519c12efb52fec7e8e32ca1dc9d11f0
Wayne <tsun0227@yahoo.com.tw>
2021-01-17 11:56:37 +0800

    black jack beta 1.0.0

--- introduction/js-practice/black-jack/index.js
+++ introduction/js-practice/black-jack/index.js
@@ -47,7 +47,7 @@ const createCard = (suit, point) => {
         default:
           display = this.point;
       }
-      return Poker.getCardImage(60, this.suit, display);
+      return Poker.getCardImage(200, this.suit, display);
     },
   };
 };
@@ -56,8 +56,14 @@ const slr = (name) => document.querySelector(name);
 
 let cardsPool = [];
 let cardsPlayer = [];
+let cardsBank = [];
 let flag = 0;
-let aceNum = 0;
+let aceNumPlayer = 0;
+let aceNumBank = 0;
+let isPlayer = true;
+let playerDone = false;
+let playerTotal = 0;
+let bankTotal = 0;
 
 //產生一副牌函式
 const initCards = () => {
@@ -71,8 +77,8 @@ const initCards = () => {
 };
 
 //呈現撲克牌函式
-const displayCard = (card) => {
-  slr("#cards").appendChild(card.toString());
+const displayCard = (id, card) => {
+  slr(id).appendChild(card.toString());
 };
 
 //洗牌函式
@@ -85,27 +91,32 @@ const shuffle = (a) => {
 };
 
 //發牌函式
-const dispatch = () => {
+const deal = () => {
   const newCard = cardsPool[flag];
-  displayCard(newCard);
-  cardsPlayer.push(newCard);
+  if (isPlayer) {
+    displayCard("#playerCards", newCard);
+    cardsPlayer.push(newCard);
+  } else {
+    displayCard("#bankCards", newCard);
+    cardsBank.push(newCard);
+  }
 
   //JQK為10點 A為11點
   if (newCard.point > 10) {
     newCard.point = 10;
   } else if (newCard.point == 1) {
     newCard.point = 11;
-    aceNum++;
+    isPlayer ? aceNumPlayer++ : aceNumBank++;
   }
   flag++;
 };
 
 //計算點數函式
-const calc = () => {
+const calc = (id, arr) => {
   let total = 0;
-  let aceNumTmp = aceNum;
+  let aceNumTmp = isPlayer ? aceNumPlayer : aceNumBank;
 
-  cardsPlayer.forEach((v) => {
+  arr.forEach((v) => {
     total += v.point;
   });
 
@@ -114,18 +125,29 @@ const calc = () => {
       for (let i = 0; i < aceNumTmp; i++) {
         total -= 10;
         if (total <= 21) {
-          slr("#total").innerHTML = total;
+          slr(id).innerHTML = total;
+          isPlayer ? (playerTotal = total) : (bankTotal = total);
           break;
         } else {
-          slr("#total").innerHTML = "bust";
+          slr(id).innerHTML = "bust";
+          if (isPlayer) {
+            isPlayer = false;
+            playerDone = true;
+          }
         }
       }
     } else {
-      slr("#total").innerHTML = "bust";
+      slr(id).innerHTML = "bust";
+      if (isPlayer) {
+        isPlayer = false;
+        playerDone = true;
+      }
     }
   } else {
-    slr("#total").innerHTML = total;
+    slr(id).innerHTML = total;
+    isPlayer ? (playerTotal = total) : (bankTotal = total);
   }
+  flag < 3 && (isPlayer = !isPlayer);
 };
 
 // const card = createCard("c", 11);
@@ -137,8 +159,73 @@ initCards();
 //洗牌
 cardsPool = [...shuffle(cardsPool)];
 
-//發票按鈕事件
-slr("#dispatch").addEventListener("click", () => {
-  dispatch();
-  calc();
+//新遊戲
+const newGame = () => {
+  slr("#deal").setAttribute("disabled", "");
+  slr("#done").setAttribute("disabled", "");
+  setTimeout(() => {
+    deal();
+    calc("#playerTotal", cardsPlayer);
+  }, 500);
+  setTimeout(() => {
+    deal();
+    calc("#bankTotal", cardsBank);
+  }, 1000);
+  setTimeout(() => {
+    deal();
+    calc("#playerTotal", cardsPlayer);
+    slr("#deal").removeAttribute("disabled", "");
+    slr("#restart").removeAttribute("disabled", "");
+    slr("#done").removeAttribute("disabled", "");
+  }, 1500);
+};
+//開始按鈕事件
+slr("#start").addEventListener("click", function () {
+  newGame();
+  this.setAttribute("disabled", "");
 });
+
+//發牌按鈕事件
+slr("#deal").addEventListener("click", () => {
+  deal();
+  isPlayer ? calc("#playerTotal", cardsPlayer) : calc("#bankTotal", cardsBank);
+});
+
+//重新開始按鈕事件
+slr("#restart").addEventListener("click", () => {
+  slr("#bankTotal").innerHTML = ``;
+  slr("#bankCards").innerHTML = ``;
+  slr("#playerTotal").innerHTML = ``;
+  slr("#playerCards").innerHTML = ``;
+  cardsPool = [...shuffle(cardsPool)];
+  cardsPlayer = [];
+  cardsBank = [];
+  flag = 0;
+  aceNumPlayer = 0;
+  aceNumBank = 0;
+  isPlayer = true;
+  newGame();
+});
+
+slr("#done").addEventListener("click", () => {
+  slr("#done").setAttribute("disabled", "");
+  slr("#deal").setAttribute("disabled", "");
+  playerDone = !playerDone;
+  isPlayer = false;
+  bankAuto();
+});
+
+const bankAuto = () => {
+  while (bankTotal < 21) {
+    if (bankTotal < playerTotal) {
+      if (slr("#bankTotal").innerHTML == "bust") {
+        break;
+      } else {
+        deal();
+        calc("#bankTotal", cardsBank);
+      }
+    } else {
+      break;
+    }
+  }
+};
